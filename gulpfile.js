7ec1eb77b6777f99e9e56db389df3833678dcdb9
SergejGenri <genrio634@gmail.com>
2017-05-15 15:30:27 +0300

    slider add

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss')
+  return gulp.src('app/scss/main.scss')
     .pipe(sass())
     .pipe(gulp.dest('app/css'))
 })
