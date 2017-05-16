d4bb0e54fed4acfc99bb7f98d785d0af33e6f30f
SergejGenri <genrio634@gmail.com>
2017-05-16 18:03:13 +0300

    add responsiv and adaptation

---
+++ app/js/plans.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+	$('.btn-buy').on('mouseover', function () {
+		$(this).prev().prev().addClass('active');
+
+		$('.btn-buy').on('mouseout', function () {
+			$(this).prev().prev().removeClass('active');
+		});
+	});
+});
