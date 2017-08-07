5a4dc8ff42cecf136e3b888fe5eb626c4e17e90c
SergejGenri <genrio634@gmail.com>
2017-08-07 12:31:26 +0300

    fix menu

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('sass', function() {
-  return gulp.src('app/scss/main.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('app/css'))
-})
+gulp.task( 'sass', function()
+{
+  gulp.src( 'app/scss/main.scss' )
+    .pipe( sass().on( 'error', function( error )
+      {
+        console.log( error );
+      } )
+    )
+    .pipe( gulp.dest( 'app/css' ) );
+} );
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']);
